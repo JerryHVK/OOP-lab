8ced4e1955947ae34a5dd0df68f9e96cc4efdd95
JerryHVK <khanghoangvan29042003@gmail.com>
2023-05-15 22:42:25 +0700

    pull

--- Lab week 3/AimsProject/src/aims/disc/DigitalVideoDisc.java
+++ Lab week 3/AimsProject/src/aims/disc/DigitalVideoDisc.java
@@ -1,9 +1,22 @@
+package aims.disc;
+
+import org.jetbrains.annotations.NotNull;
+
 public class DigitalVideoDisc {
     private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
+
+    private static int nbDigitalVideoDisc = 0;
+
+    private int id;
+
+
+    public void setTitle(String title){
+        this.title = title;
+    }
 
     public String getTitle() {
         return title;
@@ -25,14 +38,18 @@ public class DigitalVideoDisc {
         return cost;
     }
 
+    public int getId() { return id; }
+
     public DigitalVideoDisc(String title) {
+        nbDigitalVideoDisc++;
+        this.id = nbDigitalVideoDisc;
         this.title = title;
 
     }
 
 //    No, it has the same method signature as the above one
 //
-//    public DigitalVideoDisc(String category){
+//    public aims.disc.DigitalVideoDisc(String category){
 //        this.category = category;
 //    }
 
@@ -51,4 +68,24 @@ public class DigitalVideoDisc {
         this(title, category, director, cost);
         this.length = length;
     }
+
+    //@Override
+    public String toString(){
+        System.out.println(id + ". DVD - " + title + " - " + category + " - "
+                + director + " - " + length + ": " + cost) ;
+        return null;
+    }
+
+
+    public boolean isMatch(String title){
+        String[] k1 = title.split(" ", -1);
+        String[] k2 = this.title.split(" ", -1);
+        for(int i=0; i<k2.length; i++){
+            for(int j=0; j<k1.length; j++){
+                if(k1[j].equalsIgnoreCase(k2[i])) return true;
+            }
+        }
+        return false;
+    }
+
 }
